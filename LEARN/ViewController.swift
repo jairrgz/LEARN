ccfab73bf8688da7ca12920461c24193a16bbe1f
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-01-06 15:44:04 -0500

    First commit

---
+++ LEARN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEARN
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 6/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
