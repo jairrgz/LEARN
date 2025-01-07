52a8b26c702e1adf61b05f8c3e5fd3336f622418
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-01-07 10:58:50 -0500

    Modified Readme with TIP and SingleViewController to be more organized, don't forget to use self inside the closures

--- LEARN/README.md
+++ LEARN/README.md
@@ -11,3 +11,67 @@
     
     cardView.frame = CGRect(x: 25, y: 250, width: 340, height: 200)
 
+
+#Respecto a la animación:
+
+##Paso 1: Usar la clase UIViewPropertyAnimator
+
+        let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 5) {
+            cardView.frame = CGRect(x: 40, y: 60, width: 340, height: 100)
+            titleLabel.frame = CGRect(x: 44, y: 270, width: 200, height: 40)
+            
+        }
+        
+        
+🔹 Duration
+
+"Determina el tiempo que deseas que dure tu animación. Por lo general, este tiempo oscila entre 0,5 y 2 segundos.
+ Para este caso, lo he configurado la duración en 0.5 segundos."
+
+🔹Damping Ratio (relación de amortiguación)
+
+"Las animaciones de primavera y rebote son ampliamente utilizadas en iOS. Para replicarlas, combinaremos la relación de amortiguación con la duración. Lo mejor de esta técnica es que es completamente personalizable. En este caso, ajustemos la relación de amortiguación a 5."
+
+##Paso 2: Dar vida a la animación
+
+   Usa el método startAnimation()
+   
+           let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 5) {
+            cardView.frame = CGRect(x: 40, y: 60, width: 340, height: 100)
+            titleLabel.frame = CGRect(x: 44, y: 270, width: 200, height: 40)
+            
+        }
+        animator.startAnimation()
+        
+
+## TIPS
+
+🔹 Usar las propiedads de tipo componentes de vista como:
+
+       // MARK: - PROPERTIES
+    let initialView = UIView()
+    let titleLabel = UILabel()
+    let nameInCardLabel = UILabel()
+    let imageView = UIImageView()
+    let cardView = UIView()
+    
+    y colocarlas en el top de la clase
+    
+   
+   
+   
+Ejemplo:
+
+class SingleViewController: UIViewController {
+    
+    // MARK: - PROPERTIES
+    let initialView = UIView()
+    let titleLabel = UILabel()
+    let nameInCardLabel = UILabel()
+    let imageView = UIImageView()
+    let cardView = UIView()
+    
+    ... Resto de código
+    
+    
+}
--- LEARN/SingleViewController.swift
+++ LEARN/SingleViewController.swift
@@ -9,58 +9,61 @@ import UIKit
 
 class SingleViewController: UIViewController {
     
+    // MARK: - PROPERTIES
+    let initialView = UIView()
+    let titleLabel = UILabel()
+    let nameInCardLabel = UILabel()
+    let imageView = UIImageView()
+    let cardView = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /// 1. Creamos una nueva vista que usaremos como la vista principal del controlador.
-        let initialView = UIView()
-        initialView.backgroundColor = .white // Establecemos el color de fondo de esta vista a amarillo.
+        /// 1. Vista que usaremos como la vista principal del controlador.
+        initialView.backgroundColor = .gray /// Establecemos el color de fondo de la vista
 
-        /// 2. Creamos un UILabel (una etiqueta de texto) que mostrará "Jair was here".
-        let titleLabel = UILabel()
+        /// 2. Label que muestra "Jair was here".
         titleLabel.frame = CGRect(x: 80, y: 16, width: 272, height: 40)
         /// Establecemos su posición y tamaño con un rectángulo (x, y, ancho, alto).
-        titleLabel.text = "Jair was here" // El texto que se mostrará en la etiqueta.
-        titleLabel.textColor = .black // Cambiamos el color del texto a negro.
-//        label.font = .systemFont(ofSize: 40)
+        titleLabel.text = "Jair was here" /// El texto que se mostrará en la etiqueta.
+        titleLabel.textColor = .black /// Cambiamos el color del texto a negro.
         titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
         
-        let nameInCardLabel = UILabel()
+        /// 3. Establecemos su posición y tamaño con un rectángulo (x, y, ancho, alto) del bael en la tarjeta con "Jair Ramírez-Gaston"
         nameInCardLabel.frame = CGRect(x: 20, y: 20, width: 170, height:  20)
         nameInCardLabel.text = "Jair Ramírez Gaston"
         nameInCardLabel.textColor = .white
-
         
-        let imageView = UIImageView()
+        /// 4. Agregeado del componente de imagen con su imagen respectiva ios11
         imageView.frame = CGRect(x: 0, y: 0, width: 340, height: 200)
         imageView.contentMode = .scaleAspectFill
         imageView.image = UIImage(named: "ios11")
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 20
         
-        /// 3. Creamos otra vista que actuará como una "tarjeta" roja.
-        let cardView = UIView()
+        /// 5. Creamos otra vista con las mismas dimensiones que la vista con imagen
         cardView.frame = CGRect(x: 25, y: 250, width: 340, height: 200)
         // Posicionamos la vista y le damos un tamaño.
-        cardView.backgroundColor = .red // Le damos un color de fondo rojo.
+        cardView.backgroundColor = .white // Le damos un color de fondo rojo.
         cardView.layer.cornerRadius = 25
         cardView.layer.shadowOpacity = 0.9
         cardView.layer.shadowColor = CGColor(red: 140/255, green: 220/255, blue: 15/255, alpha: 0.8) // Este es ese verde limon aprox
         cardView.layer.shadowOffset = CGSize(width: 0 , height: 10)
 
-        /// 4. Agregamos las sub-vistas (el label y la tarjeta) dentro de la vista inicial.
-        initialView.addSubview(titleLabel) // Agregamos el label a la vista principal.
-        initialView.addSubview(cardView) // Agregamos la tarjeta a la vista principal.
-        cardView.addSubview(imageView)
-        cardView.addSubview( nameInCardLabel)
+        /// 6. Agregamos las sub-vistas
+        initialView.addSubview(titleLabel) /// Agregamos el label a la vista principal.
+        initialView.addSubview(cardView) ///Agregamos la tarjeta a la vista principal.
+        cardView.addSubview(imageView) /// Agregamos la vista de imagen con su imagen en la tarjeta
+        cardView.addSubview(nameInCardLabel) /// Agregamos  el nombre de la tarjeta en la tarjeta
         
-        /// 5. Finalmente, configuramos la vista principal del controlador como la nueva vista inicial.
+        /// 7. Finalmente, configuramos la vista principal del controlador como la nueva vista inicial.
         self.view = initialView
         
         /// 6.  Agregando animación
         let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 5) {
-            cardView.frame = CGRect(x: 40, y: 60, width: 340, height: 100)
-            titleLabel.frame = CGRect(x: 44, y: 270, width: 200, height: 40)
+            self.cardView.frame = CGRect(x: 40, y: 60, width: 340, height: 100)
+            self.titleLabel.frame = CGRect(x: 44, y: 270, width: 200, height: 40)
             
         }
         
